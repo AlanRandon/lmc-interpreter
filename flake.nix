7bda6b18dc8f6d11bd8d6b8ba35d2c2fe36a01af
Alan Randon <alan.randon@outlook.com>
2025-01-28 20:18:42 +0000

    feat: add nix packages

--- flake.nix
+++ flake.nix
@@ -19,26 +19,54 @@
     };
   };
   outputs =
-    { nixpkgs, zig-overlay, zls-overlay, ... }:
-    let
-      system = "x86_64-linux";
-      zig = zig-overlay.packages.x86_64-linux.master;
-      overlays = [
-        (final: prev: {
-          inherit zig;
-        })
-      ];
-      pkgs = import nixpkgs { inherit system overlays; };
-      zls = zls-overlay.packages.x86_64-linux.zls.overrideAttrs (old: {
-        nativeBuildInputs = [ zig ];
-      });
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = [
-          zls
-          zig
+    { nixpkgs, zig-overlay, zls-overlay, flake-utils, self, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        zig = zig-overlay.packages.${system}.master;
+        overlays = [
+          (final: prev: {
+            inherit zig;
+          })
         ];
-      };
-    };
+        pkgs = import nixpkgs { inherit system overlays; };
+        zls = zls-overlay.packages.${system}.zls.overrideAttrs (old: {
+          nativeBuildInputs = [ zig ];
+        });
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
+          src = ./.;
+          name = "lmc-tools";
+          nativeBuildInputs = [ zig pkgs.pkg-config ];
+          phases = "unpackPhase installPhase";
+          installPhase = ''
+            	  mkdir -p .cache
+            	  zig build install -Doptimize=ReleaseSafe --prefix $out --cache-dir $(pwd)/.zig-cache --global-cache-dir $(pwd)/.cache'';
+        });
+
+        apps.lmc-dbg = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/lmc-dbg";
+        };
+
+        apps.lmci = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/lmci";
+        };
+
+        apps.lmc-as = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/lmc-as";
+        };
+
+        apps.default = self.apps.${system}.lmc-dbg;
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            zls
+            zig
+          ];
+        };
+      });
 }
